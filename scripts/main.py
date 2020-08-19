957d5911c6c1b6732a3f1abba4758b69736a992c
Dominik Linzner <dominik.linzner@bcs.tu-darmstadt.de>
2020-08-19 10:24:19 +0200

    first processing runs (all patients jointly)

--- scripts/main.py
+++ scripts/main.py
@@ -37,11 +37,40 @@ if __name__ == '__main__':
         Q[i, i] = -sum(Q[i, :])
     #generate random initial state
     p0 = np.ones((1,D)).flatten()/D
-    print(sum(p0))
     #prior assumption on observation model
-    mu = np.arange(0,D,100)
-    sig= np.ones((D))*0.2
+    mu = np.arange(0,D)
+    sig= np.ones((D))*1
     params = (mu,sig)
 
     #init ctmc
     mc = ctmc(Q,p0,alpha,beta,T,dt,params)
+
+    dat = []
+    for j in range(0,len(patients)):
+        dat.append((patients[j].traj_p[0]-T_min,patients[j].traj_p)[1])
+
+    M = 100  # number of EM iterations
+    mc.reset_stats()
+    mc.estimate_Q()
+    print(2)
+    for m in range(0, M):
+        llh, sols = mc.process_emissions(dat)
+        mc.update_obs_model(sols, dat)
+        mc.estimate_Q()
+
+        # log-likelihood
+        print("log-likelihood:\n %s" % llh)
+        # current obs params estimate
+        print("mu_estimate:\n %s" % mc.params[0])
+        print("mu_truth:\n %s" % np.array([0, 1]))
+
+        mc.reset_stats()
+
+        a = copy.deepcopy(mc.Q_estimate)
+        b = copy.copy(mc.Q)
+        a0 = copy.deepcopy(mc.Q_estimate)
+        np.fill_diagonal(a0, 0)
+        np.fill_diagonal(b, 0)
+        # mse of rate matrix estimate
+        print("Q_estimate:\n %s" % a0)
+        print("Q_truth:\n %s" % b)
